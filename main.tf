ae2a295d5697cac8d29c89d40cec8df2f0d32a82
Sasano63 <sasano@hashicorp.com>
2021-09-28 10:49:28 +0200

    w/o default tags

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "sasano"
     workspaces {
-      name = "sasano-AWS-demostack"
+      name = "terraform-aws-demostack"
     }
   }
 }
@@ -37,13 +37,7 @@ data "terraform_remote_state" "dns" {
 //--------------------------------------------------------------------
 
 provider "aws" {
-  region = var.primary_region
-  #  alias   = "primary"
-  default_tags {
-    tags = {
-      tags = local.common_tags
-    }
-  }
+  region = var.region
 }
 
 
