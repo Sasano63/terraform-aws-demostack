d46204d48e257c5c1f5d5ed0a70ba772abb8c715
Sasano63 <sasano@hashicorp.com>
2021-09-28 10:05:23 +0200

    org

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "emea-se-playground-2019"
+    organization = "sasano"
     workspaces {
-      name = "GUY-AWS-demostack"
+      name = "sasano-AWS-demostack"
     }
   }
 }
@@ -16,9 +16,9 @@ data "terraform_remote_state" "tls" {
   backend = "remote"
   config = {
     hostname     = "app.terraform.io"
-    organization = "emea-se-playground-2019"
+    organization = "sasano"
     workspaces = {
-      name = "tls-root-certificate"
+      name = "terraform-tls-certificate"
     }
   } //config
 }
@@ -28,9 +28,9 @@ data "terraform_remote_state" "dns" {
 
   config = {
     hostname     = "app.terraform.io"
-    organization = "emea-se-playground-2019"
+    organization = "sasano"
     workspaces = {
-      name = "Guy-DNS-Zone"
+      name = "terraform-dns-multicloud"
     }
   } //network
 }
